49d2f2cbda8b59b1e103565c9253d2f5ba40c5e7
Raghav893 <raghavarora2309@gmail.com>
2026-03-28 00:00:39 +0530

    starting producer app

---
+++ producerApp/src/main/java/com/raghav/producerapp/Course.java
@@ -0,0 +1,15 @@
+package com.raghav.producerapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Course {
+    private String id;
+    private String title;
+    private String trainer;
+    private double price;
+}
