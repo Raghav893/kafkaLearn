5fb72cce0fa3adcf5ec410602b230dcffccc3164
Raghav893 <raghavarora2309@gmail.com>
2026-03-28 12:22:38 +0530

    service and  controller

---
+++ producerApp/src/main/java/com/raghav/producerapp/CourseController.java
@@ -0,0 +1,10 @@
+package com.raghav.producerapp;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/")
+public class CourseController {
+
+}
---
+++ producerApp/src/main/java/com/raghav/producerapp/CourseService.java
@@ -0,0 +1,7 @@
+package com.raghav.producerapp;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+}
