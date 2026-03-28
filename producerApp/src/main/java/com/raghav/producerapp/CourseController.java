f6ff7626224434413301e8bbbcab54ce50d2b1f3
Raghav893 <raghavarora2309@gmail.com>
2026-03-28 12:42:09 +0530

    add Kafka integration with producer service and controller

--- producerApp/src/main/java/com/raghav/producerapp/CourseController.java
+++ producerApp/src/main/java/com/raghav/producerapp/CourseController.java
@@ -1,10 +1,26 @@
 package com.raghav.producerapp;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/kafka")
 public class CourseController {
+
+    private final CourseService service;
+
+    public CourseController(CourseService service) {
+        this.service = service;
+    }
+    @PostMapping("/add-course")
+    public ResponseEntity<String> addCourse(@RequestBody Course course)
+    {
+        String response=service.sendMessage(course);
+        return ResponseEntity.ok(response);
+    }
 
 }
