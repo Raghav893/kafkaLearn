25ba3760a794ed163538fa458707fc0464d248fa
Raghav893 <raghavarora2309@gmail.com>
2026-03-28 12:45:01 +0530

    initialize consumer application with Spring Boot

---
+++ consumerApp/src/test/java/com/raghav/consumerapp/ConsumerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghav.consumerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
